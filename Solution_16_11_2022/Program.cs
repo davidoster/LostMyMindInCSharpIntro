a3525efab6dc45a8ea39fe175bdcd8d0a1fb7162
David Oster aka George Pasparakis <paspa@hotmail.com>
2022-11-16 13:41:10 +0200

    Add project files.

---
+++ Solution_16_11_2022/Program.cs
@@ -0,0 +1,70 @@
+﻿using Solution_16_11_2022.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution_16_11_2022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Step 1. Ask from the user to type an input
+            // Step 2. Store the input to a string variable
+            // Step 3. Convert the value of the variable to the appropriate type
+            //Console.WriteLine("Please type the Id: ");
+            //string id = Console.ReadLine();
+            //int iId = Convert.ToInt16(id);
+
+            // Step 4. Create as many methods as required to get the different types (AsString, AsInt, AsDouble)
+            // do the method(s)
+
+            //Product myProduct = new Product(GetPropertyAsInt("Id"), GetPropertyAsString("Title"), 
+            //                                GetPropertyAsString("Description"), GetPropertyAsDouble("Price"), 
+            //                                GetPropertyAsString("Serial Number"));
+            //Console.WriteLine(myProduct);
+
+            // Step 5. Moved these methods to their own class called ConsolePrompt
+            //Product myProduct2 = new Product(ConsolePrompt.GetPropertyAsInt("Id"), 
+            //    ConsolePrompt.GetPropertyAsString("Title"), ConsolePrompt.GetPropertyAsString("Description"),
+            //    ConsolePrompt.GetPropertyAsDouble("Price"), ConsolePrompt.GetPropertyAsString("Serial Number"));
+
+            //int[] array = new int[2];
+            //array[0] = 1;
+            //array[1] = 2;
+            //Console.WriteLine(array[0]);
+            //Console.WriteLine(array[1]);
+            //Console.WriteLine(array.Length);
+
+            Product[] products = CreateProducts(2);
+
+            // print array of products
+            for (int i = 0; i < products.Length; i++)
+            {
+                Console.WriteLine(products[i]);
+            }
+            Console.WriteLine("Press any key to continue....");
+            Console.ReadKey();
+
+        }
+
+        private static Product[] CreateProducts(int noProducts)
+        {
+            int noOfProducts = noProducts;
+            Product[] products = new Product[noOfProducts];
+            for (int i = 0; i < noOfProducts; i++)
+            {
+                products[i] = new Product(ConsolePrompt.GetPropertyAsInt("Id"),
+                ConsolePrompt.GetPropertyAsString("Title"), ConsolePrompt.GetPropertyAsString("Description"),
+                ConsolePrompt.GetPropertyAsDouble("Price"), ConsolePrompt.GetPropertyAsString("Serial Number"));
+            }
+
+            return products;
+        }
+
+
+    }
+}
